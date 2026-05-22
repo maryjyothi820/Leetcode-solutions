b95d2a7cb54eaa75c6579e1753a92268e48b49ad
maryjyothi820 <kandulajyothi328@gmail.com>
2026-05-22 21:43:37 +0530

    Add files via upload

---
+++ Day-38.py
@@ -0,0 +1,12 @@
+198.House_robber
+class Solution:
+    def rob(self, nums):
+        prev1 = 0
+        prev2 = 0
+        
+        for money in nums:
+            temp = max(prev1, prev2 + money)
+            prev2 = prev1
+            prev1 = temp
+        
+        return prev1
