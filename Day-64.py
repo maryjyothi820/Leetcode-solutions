0875b5dec849feb9278feb25da85a5711586bc63
maryjyothi820 <kandulajyothi328@gmail.com>
2026-06-18 10:52:22 +0530

    Day-64.py

---
+++ Day-64.py
@@ -0,0 +1,19 @@
+539.minimum_time_difference.py
+class Solution:
+    def findMinDifference(self, timePoints):
+        minutes = []
+
+        for t in timePoints:
+            h, m = map(int, t.split(":"))
+            minutes.append(h * 60 + m)
+
+        minutes.sort()
+
+        ans = float('inf')
+
+        for i in range(1, len(minutes)):
+            ans = min(ans, minutes[i] - minutes[i - 1])
+
+        ans = min(ans, 1440 - minutes[-1] + minutes[0])
+
+        return ans
