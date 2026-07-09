773fc52cc46c1d2d285c1e2164baa9d0339b844a
maryjyothi820 <kandulajyothi328@gmail.com>
2026-07-09 07:29:15 +0530

    Day-77.py

---
+++ Day-77.py
@@ -0,0 +1,15 @@
+1015. Smallest Integer Divisible by K.py
+class Solution:
+    def smallestRepunitDivByK(self, k):
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+
+        remainder = 0
+
+        for length in range(1, k + 1):
+            remainder = (remainder * 10 + 1) % k
+
+            if remainder == 0:
+                return length
+
+        return -1
