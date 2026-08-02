8a9bd24ca12416128224ae28dd6b457443d4b361
maryjyothi820 <kandulajyothi328@gmail.com>
2026-08-02 21:11:03 +0530

    Day-97.py

---
+++ Day-97.py
@@ -0,0 +1,11 @@
+448. Find All Numbers Disappeared in an Array.py
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        s = set(nums)
+        ans = []
+
+        for i in range(1, len(nums) + 1):
+            if i not in s:
+                ans.append(i)
+
+        return ans
