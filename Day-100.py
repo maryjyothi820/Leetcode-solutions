f6a571b47ebd63a16db13fb06dc5cda53219594c
maryjyothi820 <kandulajyothi328@gmail.com>
2026-08-10 13:10:45 +0530

    Day-100.py

---
+++ Day-100.py
@@ -0,0 +1,24 @@
+1679. Max Number of K-Sum Pairs.py
+class Solution:
+    def maxOperations(self, nums, k):
+        nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+        operations = 0
+
+        while left < right:
+            total = nums[left] + nums[right]
+
+            if total == k:
+                operations += 1
+                left += 1
+                right -= 1
+
+            elif total < k:
+                left += 1
+
+            else:
+                right -= 1
+
+        return operations
